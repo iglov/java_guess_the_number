1957789cd9393e5c923fac2c7b52a7d61a8b4c12
iglov <iglov@xakep.ru>
2017-05-09 07:19:54 -0400

    Add cycle and 3 levels

--- src/ru/geekbrains/guess_the_number/Main.java
+++ src/ru/geekbrains/guess_the_number/Main.java
@@ -4,9 +4,19 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        System.out.println("Welcome to \"Guess the number!\"");
+        for (int range = 10; range <= 30; range += 10) while_guess(range);
+        System.out.println("Game over! You win!");
+        scanner.close();
+    }
+
     private static Scanner scanner = new Scanner(System.in);
 
-    private static void while_guess (int range, int number) {
+    private static void while_guess (int range) {
+        int number = (int)(Math.random() * range);
+        System.out.println("I made a number between 0 and " + range + ". Please, guess the number! ^_^");
+
         while(true) {
             System.out.println("What number is? Type it ...");
             int input_number = scanner.nextInt();
@@ -16,19 +26,9 @@ public class Main {
             } else if (input_number > number) {
                 System.out.println("Your number grow then my. Try again ;)");
             } else if (input_number == number) {
-                System.out.println("Congratulation! You guessed!");
+                System.out.println("Congratulation! Level complete!");
                 break;
             }
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to \"Guess the number!\"");
-
-        int range = 15;
-        int number = (int)(Math.random() * range);
-        System.out.println("I made a number from 1 to " + range + ". Please, guess the number! ^_^");
-        while_guess(range, number);
-        scanner.close();
     }
 }
